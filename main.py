a1cd6ed3bd15977f18c98ae42bd796cddc2cd5f7
Florent Audonnet <2330834a@student.gla.ac.uk>
2019-11-09 23:49:24 +0000

    Wip Added a better notification for no DM3 files Modification to #47

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from ui_mainwindow import Ui_MainWindow
 matplotlib.use('Qt5Agg')
 progname = os.path.basename(sys.argv[0])
 progversion = "0.1"
+os.environ["OMP_NUM_THREADS"] = "1"
 
 
 class ApplicationWindow(QMainWindow):
@@ -43,15 +44,16 @@ class ApplicationWindow(QMainWindow):
     def function_hdf5(self):
         fname, fother = QFileDialog.getOpenFileName(
             self, 'Open file', self.last_path, "HDF5 (*.hdf5)")
-        self.last_path = fname
-        #ask whats the point of doing that 
-        self.f = h5py.File(fname, 'r')
-        self.ds = self.f['fpd_expt/fpd_data/data']
-        self.sum_im = self.f['fpd_expt/fpd_sum_im/data'].value
-        self.sum_dif = self.f['fpd_expt/fpd_sum_dif/data'].value
-        # since it is never used
-        b = DataBrowserNew(fname, widget_1=self.ui.widget_3,
-                        widget_2=self.ui.widget_4)
+        if fname:
+            self.last_path = fname
+            #ask whats the point of doing that 
+            self.f = h5py.File(fname, 'r')
+            self.ds = self.f['fpd_expt/fpd_data/data']
+            self.sum_im = self.f['fpd_expt/fpd_sum_im/data'].value
+            self.sum_dif = self.f['fpd_expt/fpd_sum_dif/data'].value
+            # since it is never used
+            b = DataBrowserNew(fname, widget_1=self.ui.widget_3,
+                            widget_2=self.ui.widget_4)
 
 
 
@@ -60,10 +62,11 @@ class ApplicationWindow(QMainWindow):
         print("print from function_dm3")
         fname, fother = QFileDialog.getOpenFileName(
             self, 'Open file', self.last_path, "Digital Micrograph files (*.dm3)")
-        self.last_path = fname
-        self.dm3_path = fname
-        self.ui.DM3.clear()
-        self.ui.DM3.insert(fname)
+        if fname:
+            self.last_path = fname
+            self.dm3_path = fname
+            self.ui.DM3.clear()
+            self.ui.DM3.insert(fname)
 
 
     @Slot()
@@ -71,29 +74,41 @@ class ApplicationWindow(QMainWindow):
         print("print from function_mib")
         fname, fother = QFileDialog.getOpenFileName(
             self, 'Open file', self.last_path, "MERLIN binary files (*.mib)")
-        self.last_path = fname
-        self.mib_path = fname
-        self.ui.MIB.clear()
-        self.ui.MIB.insert(fname)
+        if fname:
+            self.last_path = fname
+            self.mib_path = fname
+            self.ui.MIB.clear()
+            self.ui.MIB.insert(fname)
 
     @Slot()
     def LoadFiles(self):
         # QUICK FIX FOR NO DM3 SUPPLIED, MORE THOROUGH FILE CHECKING TO-DO
+        x_value = None
+        y_value = None
         try:
             dm3 = self.dm3_path
         except AttributeError:
             dm3 = []
+            response = QtWidgets.QMessageBox.warning(
+                self, "Warning", "<strong>We noticed you don't have a Digital Micrograph files</strong> <br> Do you want to select one ?",
+                QtWidgets.QMessageBox.Cancel | QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                QtWidgets.QMessageBox.Yes)
+            if response == QtWidgets.QMessageBox.Cancel:
+                return
+            elif response == QtWidgets.QMessageBox.Yes:
+                self.function_dm3()  # load a .DM3 file and use it
+                dm3 = self.dm3_path
+            else:  # load the data using custum parameter
+                print("No")
+                x = 256
+                y = 256
+                x_value = (x, 'x', 'na')
+                y_value = (y, 'y', 'na')
+
         mib = self.mib_path
         hdr = self.mib_path[:-4]+".hdr"
-        if dm3:
-            # with dm3
-            print("working with dm3")
-            self.mb = MerlinBinary(mib, hdr, dm3, row_end_skip=1)
-        else:
-            # without dm3
-            print("working without dm3")
-            self.mb = MerlinBinary(mib, hdr, dm3, scanYalu=(256, 'y', 'na'),
-                                   scanXalu=(256, 'x', 'na'), row_end_skip=1)
+        self.mb = MerlinBinary(mib, hdr, dm3, scanYalu=y_value,
+                                   scanXalu=x_value, row_end_skip=1)
         
         self.ds = self.mb.get_memmap()
         real_skip = 8
@@ -115,13 +130,14 @@ class ApplicationWindow(QMainWindow):
         #compute summed real space image
         #ask what the point of this one
         self.sum_dif = fpdp.sum_dif(self.ds_sel, 16, 16)
-        b = DataBrowserNew(self.ds_sel, nav_im=sum_im,
+        b = DataBrowserNew(self.ds_sel, nav_im=self.sum_im,
                            widget_1=self.ui.widget_3, widget_2=self.ui.widget_4)
 
 
     def function_about(self):
-        QtWidgets.QMessageBox.about(self, "About")
-
+        self.about = QtWidgets.QMessageBox()
+        self.about.setText("This is an about box that should be filed")
+        self.about.exec()
 
 qApp = QtWidgets.QApplication(sys.argv)
 
