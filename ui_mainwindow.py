e991e21a930832386304d07bc7b951d58c45c806
Florent Audonnet <2330834a@student.gla.ac.uk>
2019-11-10 20:00:11 +0000

    Added safety check and improve reusability
    If you close the 2 input widget it automaticaly revert to the default value

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'mainwindow.ui',
 # licensing of 'mainwindow.ui' applies.
 #
-# Created: Sun Nov 10 19:04:36 2019
+# Created: Sun Nov 10 19:58:29 2019
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
