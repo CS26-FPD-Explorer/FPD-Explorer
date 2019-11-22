f3e6396d3e74a40cd2b9df67240cd3fe6c9aaac9
Bruce <2316957k@student.gla.ac.uk>
2019-11-22 23:13:41 +0000

    change python version to dev

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'mainwindow.ui',
 # licensing of 'mainwindow.ui' applies.
 #
-# Created: Wed Nov 13 17:57:01 2019
+# Created: Fri Nov 22 23:01:40 2019
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
