7252d479113ebadd86a1319a094bcbda85167b75
Florent Audonnet <2330834a@student.gla.ac.uk>
2019-11-29 15:28:03 +0000

    Updated the compile ui

--- compile_ui.py
+++ compile_ui.py
@@ -1,12 +1,17 @@
 # Dirty file to automatically compile all .ui files
 
 from os import walk
+import subprocess
+
 import os
 f = []
-for (dirpath, dirnames, filenames) in walk("./"):
-    f.extend(filenames)
-    break
+for (dirpath, dirnames, filenames) in walk("./resources/"):
+    f.extend([os.path.join(*dirpath.split("/"), s) for s in filenames])
+
+print(f"Found {len(f)} files: Compiling.....")
 for el in f:
-    splitted = el.split(".")
-    if splitted[1] == "ui":
-        os.system("pyside2-uic " + el + " > ui_" + splitted[0] + ".py")
+    file_path, ext = os.path.splitext(el)
+    file = os.path.split(file_path)[-1]
+    if ext == ".ui":
+        print(f"Compiling {el} to ui_{file}.py ... ")
+        subprocess.run("pyside2-uic -o " + "ui_" + file + ".py " + el , capture_output=True, check=True)
