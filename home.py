db5e9e0b4b1abede83b39e9b6dac0f12a98aa79d
Michal Broos <2330994b@student.gla.ac.uk>
2019-12-18 13:14:01 +0000

    Updated imports to reflect the new structure of project

--- home.py
+++ home.py
@@ -1,33 +1,25 @@
-from __future__ import unicode_literals
 import sys
 import os
 import matplotlib
-import random
-import numpy as np
-import scipy as sp
 import h5py
 
-from PySide2 import QtWidgets, QtCore, QtGui
+from PySide2 import QtWidgets, QtCore
 from PySide2.QtWidgets import QMainWindow, QFileDialog
 from PySide2.QtCore import Slot
 
-from fpd.fpd_file import MerlinBinary
+from resources.ui_homescreen import Ui_MainWindow
+# from resources.ui_mainwindow import Ui_MainWindow
 
-from ui_homescreen import Ui_MainWindow
-from ui_inputbox import Ui_InputBox
-# from ui_mainwindow import Ui_MainWindow
-from DataBrowserNew import DataBrowserNew
+from data_browser_new import DataBrowserNew
 from custom_widgets import *
+import data_browser_explorer
+from collections import OrderedDict
 
 os.environ["OMP_NUM_THREADS"] = "1"
 
 # Make sure that we are using QT5
 
-# Uncomment this line before running, it breaks sphinx-gallery builds
-# from PyQt5 import QtCore, QtWidgets
 matplotlib.use('Qt5Agg')
-# progname = os.path.basename(sys.argv[0])
-progversion = "0.1"
 os.environ["OMP_NUM_THREADS"] = "1"
 
 
