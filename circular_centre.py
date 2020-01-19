1d6e55095adef308a833bb2796d5cf3a9baaa69e
Michal Broos <2330994b@student.gla.ac.uk>
2020-01-19 14:51:02 +0000

    Changed flow control logic for find_circ_centre
    To prevent displaying input form only to then inform the user that
    files have not been loaded and they cannot proceed.

--- circular_centre.py
+++ circular_centre.py
@@ -15,15 +15,15 @@ def find_circular_centre(ApplicationWindow):
     Calculate the circular center for the current data
     """
     
-    widget=CustomInputFormCircularCenter()
-    widget.exec()
-    sigma=widget._ui.sigma_value.value()
-    rmms_1=widget._ui.rmms1st.value()
-    rmms_2=widget._ui.rmms2nd.value()
-    rmms_3=widget._ui.rmms3rd.value()
-    print(sigma,rmms_1,rmms_2,rmms_3)
-    ##lowest_input_radius,max_radius,number_of_circles
     if ApplicationWindow._files_loaded:
+        widget=CustomInputFormCircularCenter()
+        widget.exec()
+        sigma=widget._ui.sigma_value.value()
+        rmms_1=widget._ui.rmms1st.value()
+        rmms_2=widget._ui.rmms2nd.value()
+        rmms_3=widget._ui.rmms3rd.value()
+        print(sigma,rmms_1,rmms_2,rmms_3)
+        ##lowest_input_radius,max_radius,number_of_circles
         ApplicationWindow.cyx,ApplicationWindow.radius = fpdp.find_circ_centre(ApplicationWindow._sum_dif,
         sigma, rmms=(rmms_1, rmms_2, rmms_3))
         print("desired code reached")
