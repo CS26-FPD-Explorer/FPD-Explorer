9b1fc6521683c037c52de1d2bfdb316b5c9fbfc4
Michal Broos <2330994b@student.gla.ac.uk>
2020-01-21 22:33:23 +0000

    Formatting updates to circular_centre.py

--- circular_centre.py
+++ circular_centre.py
@@ -5,11 +5,11 @@ import fpd.fpd_processing as fpdp
 
 from custom_widgets import CustomInputFormCircularCenter
 
+# need to go through private variables
 def find_circular_centre(ApplicationWindow):
     """
     Calculate the circular center for the current data
     """
-    
     if ApplicationWindow._files_loaded:
         widget=CustomInputFormCircularCenter()
         widget.exec()
@@ -17,11 +17,8 @@ def find_circular_centre(ApplicationWindow):
         rmms_1=widget._ui.rmms1st.value()
         rmms_2=widget._ui.rmms2nd.value()
         rmms_3=widget._ui.rmms3rd.value()
-        print(sigma,rmms_1,rmms_2,rmms_3)
-        ##lowest_input_radius,max_radius,number_of_circles
         ApplicationWindow.cyx,ApplicationWindow.radius = fpdp.find_circ_centre(ApplicationWindow._sum_dif,
         sigma, rmms=(rmms_1, rmms_2, rmms_3))
-        print("desired code reached")
     else:
         QtWidgets.QMessageBox.warning(ApplicationWindow,"Warning",
-        "The files must be loaded before the circular center can be calculated.")
+        "<b>The files must be loaded</b> before the circular center can be calculated.")
