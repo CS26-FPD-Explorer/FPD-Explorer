bffeb54353d21cec3a4871dd79e9a673e4ad6414
brucekerr <2316957k@student.gla.ac.uk>
2020-01-29 15:54:46 +0000

    Updating imports and sys file path to follow testing conventions

--- tests/test_inputbox.py
+++ tests/test_inputbox.py
@@ -1,5 +1,7 @@
 import pytest
-from custom_widgets import CustomInputForm
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../")
+from fpd_explorer.custom_widgets import CustomInputForm
 
 def init_inputbox():
     """
