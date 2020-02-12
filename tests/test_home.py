5c99386ee13bd65827e61b5b5795f96f06ef6660
Ewan Pandelus <2319069P@student.gla.ac.uk>
2020-02-12 13:52:58 +0000

    Auto Formatted to comply with pep8 Standard

--- tests/test_home.py
+++ tests/test_home.py
@@ -2,7 +2,8 @@ import pytest
 from pytestqt import qtbot
 from PySide2 import QtCore
 from PySide2.QtCore import QTimer, QCoreApplication
-import sys, os
+import sys
+import os
 from fpd_explorer.data_browser_explorer import DataBrowserWidget
 from fpd_explorer.home import *
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../")
@@ -14,10 +15,10 @@ def test_clear_files(qtbot):
     aw.clear_files()
     assert aw._files_loaded == False
     assert aw._cyx == None
-    assert aw._ap == None 
+    assert aw._ap == None
     with pytest.raises(AttributeError) as excinfo:
-         aw._mib_path is None == True 
-         aw._dm3_path is None == True
-         assert "self._mib_path and self._dm3_path have been deleted" in str(excinfo.value)
+        aw._mib_path is None == True
+        aw._dm3_path is None == True
+        assert "self._mib_path and self._dm3_path have been deleted" in str(excinfo.value)
     assert aw._ui.mib_line.text() == ''
     assert aw._ui.dm3_line.text() == ''
