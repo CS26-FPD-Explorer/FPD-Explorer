d43f41427f0355d8d7480303390b9b05d656a063
Florent Audonnet <2330834a@student.gla.ac.uk>
2020-02-16 16:05:20 +0000

    Refactoring to use private variable Refactored missing imports

--- fpd_explorer/gui_generator.py
+++ fpd_explorer/gui_generator.py
@@ -32,16 +32,18 @@ class UI_Generator(QtWidgets.QDialog):
 
     """
 
-    def __init__(self, application_window, fnct, items_per_column=10):
+    def __init__(self, application_window, fnct, items_per_column=10, key_ignore=None, key_add=None):
         super(UI_Generator, self).__init__()
         self.application_window = application_window
-        self.param = self.get_param(fnct)
+        self.key_ignore = key_ignore
+        self.key_add = key_add
+        self.param = self._get_param(fnct)
         self.items_per_column = items_per_column
         self.default = {}
-        self.setup_ui()
+        self._setup_ui()
         self.setWindowFlags((self.windowFlags() | Qt.MSWindowsFixedSizeDialogHint) & ~Qt.WindowContextHelpButtonHint)
 
-    def get_param(self, fnct):
+    def _get_param(self, fnct):
         sig = signature(fnct)
         doc = fnct.__doc__
         result = {}
@@ -71,9 +73,13 @@ class UI_Generator(QtWidgets.QDialog):
                     if len(result[current_name]) > 2:
                         result[current_name][2] = " ".join(result[current_name][2:])
                         del result[current_name][3:]
+        if self.key_ignore is not None:
+            [result.pop(x, None) for x in self.key_ignore]
+        if self.key_add is not None:
+            [result.update(self.key_add)]
         return result
 
-    def setup_ui(self):
+    def _setup_ui(self):
         self.result = {}
         """
         self.widgets is a dictionary with the type as key and a list of widgets as val
@@ -96,23 +102,23 @@ class UI_Generator(QtWidgets.QDialog):
             elif "str" in val[0]:
                 default_val = val[1] if val[1] is not None else key
                 widget = QLineEdit()
-                widget.setPlaceholderText(self.set_default(widget, default_val))
+                widget.setPlaceholderText(self._set_default(widget, default_val))
                 param_type = "str"
             elif "int" in val[0] or "scalar" in val[0] or "float" in val[0]:
-                widget = self.create_int_float(val, True if "float" in val[0] else False)
+                widget = self._create_int_float(val, True if "float" in val[0] else False)
                 param_type = "int"
 
             elif "bool" in val[0]:
                 default_val = val[1] if val[1] is not None else False
                 widget = QCheckBox()
-                widget.setChecked(self.set_default(widget, default_val))
+                widget.setChecked(self._set_default(widget, default_val))
                 param_type = "bool"
             elif "iterable" in val[0]:
                 iter_ran = int(''.join(x for x in val[0] if x.isdigit()))
                 widget = QtWidgets.QWidget()
                 lay = QVBoxLayout()
                 for el in range(iter_ran):
-                    lay.addWidget(self.create_int_float(val))
+                    lay.addWidget(self._create_int_float(val))
                 widget.setLayout(lay)
                 param_type = "iterable_" + str(iter_ran)
 
@@ -120,29 +126,27 @@ class UI_Generator(QtWidgets.QDialog):
                 print("TODO : Implement : ", val[0])
                 continue
             none_possible = False
-            print(val)
             if "None" in val[0]:
                 none_possible = True
             widget.setToolTip(val[2])
             self.widgets[param_type].append([key, widget, none_possible])
-        self.format_layout()
+        self._format_layout()
 
-    def create_int_float(self, val, is_float=False):
+    def _create_int_float(self, val, is_float=False):
         default_val = val[1] if val[1] is not None else 0
         if is_float:
             widget = QDoubleSpinBox()
         else:
             widget = QSpinBox()
-        widget.setValue(self.set_default(widget, default_val))
+        widget.setValue(self._set_default(widget, default_val))
         widget.setMinimum(-1000)
         widget.setMaximum(1000)
         return widget
 
-    def save(self):
+    def _save(self):
         for param_type, widgets in self.widgets.items():
             for key, widget, none_possible in widgets:
                 if param_type == "bool":
-                    print(key,widget.isChecked() )
                     self.result[key] = widget.isChecked()
                 elif param_type == "int":
                     tmp = widget.value()
@@ -157,17 +161,17 @@ class UI_Generator(QtWidgets.QDialog):
                     self.result[key] = val_ls
                 else:
                     self.result[key] = widget.text()
-        print(self.result)
+        # print(self.result)
         self.accept()
 
     def get_result(self):
         return self.result
 
-    def set_default(self, widget, default_val):
+    def _set_default(self, widget, default_val):
         self.default[widget] = default_val
         return default_val
 
-    def restore_default(self):
+    def _restore_default(self):
         for param_type, widgets in self.widgets.items():
             for key, widget, none_possible in widgets:
                 if param_type == "bool":
@@ -182,7 +186,7 @@ class UI_Generator(QtWidgets.QDialog):
                     widget.clear()
                     widget.setPlaceholderText(self.default[widget])
 
-    def format_layout(self):
+    def _format_layout(self):
         # Create layout and add widgets
         all_widget = []
         self.sub_ls = defaultdict(list)
@@ -204,22 +208,21 @@ class UI_Generator(QtWidgets.QDialog):
                 else:
                     all_widget.append((key.replace("_", " ").capitalize(), widget))
                 # self.layout.addRow(key.replace("_", " ").capitalize(), widget)
-        print(self.sub_ls)
         layout = QGridLayout()
 
-        self.create_colums(all_widget, layout)
+        self._create_colums(all_widget, layout)
 
         buttonBox = QDialogButtonBox(QDialogButtonBox.Save |
                                      QDialogButtonBox.Cancel |
                                      QDialogButtonBox.RestoreDefaults)
-        buttonBox.accepted.connect(self.save)
+        buttonBox.accepted.connect(self._save)
         buttonBox.rejected.connect(self.reject)
-        buttonBox.button(QDialogButtonBox.RestoreDefaults).clicked.connect(self.restore_default)
+        buttonBox.button(QDialogButtonBox.RestoreDefaults).clicked.connect(self._restore_default)
         layout.addWidget(buttonBox, 1, 0, 1, len(all_widget) // self.items_per_column + 1, Qt.AlignRight)
         self.setLayout(layout)
         self.setFixedSize(self.minimumWidth(), self.minimumHeight())
 
-    def create_colums(self, widget_list, vert_layout, n=0):
+    def _create_colums(self, widget_list, vert_layout, n=0):
         tmp = QtWidgets.QWidget()
         layout = QFormLayout()
         for name, widget in widget_list[0 + self.items_per_column *
@@ -230,4 +233,4 @@ class UI_Generator(QtWidgets.QDialog):
         vert_layout.addWidget(tmp, 0, n)
         if self.items_per_column * (n + 1) >= len(widget_list):
             return
-        self.create_colums(widget_list, vert_layout, n=n + 1)
+        self._create_colums(widget_list, vert_layout, n=n + 1)
