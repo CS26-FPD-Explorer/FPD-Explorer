3fc8f16044dc8b5bb16e71ae70cfdb83413f85c0
Florent Audonnet <2330834a@student.gla.ac.uk>
2020-03-06 03:10:14 +0000

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [] 
+if os.path.isfile("requirements.txt"):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="fpd-explorer",  
+    version="1.0",
+    author="Bruce Kerr,Ewan Pandelus,Florent Audonnet, Michal Broos,Ruize Shen ",
+    author_email="author@example.com",
+    description="An explorer ui for the fpd library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://stgit.dcs.gla.ac.uk./tp3-2019-cs26/cs26-main",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
