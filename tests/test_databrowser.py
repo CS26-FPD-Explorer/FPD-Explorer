2e83cbfd1dc4a2b35063b3941368e28bd7f0dcc8
brucekerr <2316957k@student.gla.ac.uk>
2020-03-09 10:27:50 +0000

    Completing merge with remote as made changes to remote on gitlab

--- tests/test_databrowser.py
+++ tests/test_databrowser.py
@@ -23,20 +23,22 @@ def test_load_files(qtbot):
     keyboard = Controller()
 
     # adding dm3 file
-    aw._dm3_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.dm3"
-    #aw._dm3_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.dm3"
+    #aw._dm3_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.dm3"
+    aw._dm3_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.dm3"
 
     # adding mib file
-    aw.mib_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.mib"
-    aw._mib_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.mib"
-    #aw.mib_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.mib"
-    #aw._mib_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.mib"
+    #aw.mib_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.mib"
+    #aw._mib_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.mib"
+
+    aw.mib_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.mib"
+    aw._mib_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.mib"
 
     def interact():
         keyboard.press(Key.enter)
 
     QTimer.singleShot(5000, interact)
     aw.load_files()
+
     assert logger.check_if_all_needed(Flags.files_loaded)
 '''
 
@@ -51,9 +53,22 @@ def test_load_hdf5(qtbot):
     aw._ui.action_hdf5.trigger()
 
     assert logger.check_if_all_needed(Flags.files_loaded)
-    
-    w.start_dbrowser()
+
+    aw.start_dbrowser()
     assert aw._ui.tabWidget.widget(1).findChild(QtWidgets.QComboBox, "colorMap").currentIndex() == 0
+
+
+def test_load_hdf5(qtbot):
+    aw = ApplicationWindow()
+    qtbot.addWidget(aw)
+    logger.setup(aw._ui.log_text, aw)
+    keyboard = Controller()
+
+    aw.hdf5_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.hdr"
+    #aw.hdf5_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\13_FeRh-Alisa_DW_diff_20um_115C.hdr"
+    aw._ui.action_hdf5.trigger()
+
+    assert logger.check_if_all_needed(Flags.files_loaded)
 
 
 def test_init_color_map(qtbot):
