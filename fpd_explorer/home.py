c1683ee7fdf943ab1fc25941ebb1f5787f5c7ec9
Michal Broos <2330994b@student.gla.ac.uk>
2020-03-09 18:48:44 +0000

    Add missing colormaps
    Adds two categories that were removed by mistake.

--- fpd_explorer/home.py
+++ fpd_explorer/home.py
@@ -97,6 +97,12 @@ class ApplicationWindow(QMainWindow):
             'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
             'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
         self.cmaps['Cyclic'] = ['twilight', 'twilight_shifted', 'hsv']
+        self.cmaps['Qualitative'] = ['Pastel1', 'Pastel2', 'Paired', 'Accent',
+                                     'Dark2', 'Set1', 'Set2', 'Set3',
+                                     'tab10', 'tab20', 'tab20b', 'tab20c']
+        self.cmaps['Miscellaneous'] = ['flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+                                       'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg',
+                                       'gist_rainbow', 'rainbow', 'jet', 'nipy_spectral', 'gist_ncar']
 
     @Slot()
     def guide_me(self, topic):
