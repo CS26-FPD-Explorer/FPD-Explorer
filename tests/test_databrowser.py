130e87d472700063a037f656a81517db300215ea
brucekerr <2316957k@student.gla.ac.uk>
2020-03-09 13:09:36 +0000

    Tiding and removing test code

--- tests/test_databrowser.py
+++ tests/test_databrowser.py
@@ -49,25 +49,12 @@ def test_load_hdf5(qtbot):
     logger.setup(aw._ui.log_text, aw)
     keyboard = Controller()
 
-    aw.hdf5_path = "/home/ubuntu/example-data/13_FeRh-Alisa_DW_diff_20um_115C.hdr"
-    aw._ui.action_hdf5.trigger()
-
-    assert logger.check_if_all_needed(Flags.files_loaded)
-
-    aw.start_dbrowser()
-    assert aw._ui.tabWidget.widget(1).findChild(QtWidgets.QComboBox, "colorMap").currentIndex() == 0
-
-
-def test_load_hdf5(qtbot):
-    aw = ApplicationWindow()
-    qtbot.addWidget(aw)
-    logger.setup(aw._ui.log_text, aw)
-    keyboard = Controller()
-
     aw.hdf5_path = "/home/ubuntu/example-data/4DSTEM_FeRh_element.hdf5"
     aw._ui.action_hdf5.trigger()
 
     assert logger.check_if_all_needed(Flags.files_loaded)
+    aw.start_dbrowser()
+    assert aw._ui.tabWidget.widget(1).findChild(QtWidgets.QComboBox, "colorMap").currentIndex() == 0
 
 
 def test_init_color_map(qtbot):
