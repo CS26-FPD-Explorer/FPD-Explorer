115819305582c05089fd2528558938f1041f59bf
brucekerr <2316957k@student.gla.ac.uk>
2020-03-11 10:42:05 +0000

    Revert "Fixing fail using hdf5 files instead"
    This reverts commit d9bb853e89ad51213a752118a12c602dfc0b92ff.

--- tests/test_databrowser.py
+++ tests/test_databrowser.py
@@ -13,7 +13,7 @@ import time
 
 from fpd_explorer.files_fncts import *
 
-'''
+
 # omitted due to memory loading error on the vm
 # for local use only
 def setup_databrowser_tests(qtbot):
@@ -21,34 +21,22 @@ def setup_databrowser_tests(qtbot):
     db = DataBrowserWidget(aw)
     qtbot.addWidget(db)
     return db
-'''
-
-
-def load_hdf5():
-    aw = ApplicationWindow()
-    qtbot.addWidget(aw)
-    logger.setup(aw._ui.log_text, aw)
-    config.load_config()
-    aw.hdf5_path = "/home/ubuntu/example-data/4DSTEM_FeRh_element.hdf5"
-    enter()
-    aw._ui.action_hdf5.trigger()
-    return aw
 
 
 def test_load_files(qtbot):
-    aw = load_hdf5()
+    aw = setup_tests(qtbot)
     assert logger.check_if_all_needed(Flags.files_loaded)
 
 
 def test_load_hdf5(qtbot):
-    aw = load_hdf5()
+    aw = setup_tests(qtbot)
     assert logger.check_if_all_needed(Flags.files_loaded)
     aw.start_dbrowser()
     assert aw._ui.tabWidget.widget(1).findChild(QtWidgets.QComboBox, "colorMap").currentIndex() == 0
 
 
 def test_load_npz(qtbot):
-    aw = load_hdf5()
+    aw = setup_tests(qtbot)
 
     aw.npz_path = "/home/ubuntu/example-data/VirtualAnnularImages_20200305_185503.npz"
     #aw.npz_path = "C:\cs26\example-data\Transfer-wbJpeYPVBcfcov9N\\VirtualAnnularImages_20200305_185503.npz"
@@ -58,15 +46,15 @@ def test_load_npz(qtbot):
 
 
 def test_init_color_map(qtbot):
-    db = load_hdf5()
+    db = setup_databrowser_tests(qtbot)
     assert db._ui.colorMap.count() == 54
 
 
 def test_get_diff(qtbot):
-    db = load_hdf5()
+    db = setup_databrowser_tests(qtbot)
     assert db.get_diff() == db._ui.diffractionWidget
 
 
 def test_get_nav(qtbot):
-    db = load_hdf5()
+    db = setup_databrowser_tests(qtbot)
     assert db.get_nav() == db._ui.navigationWidget
