46af3fd3342c9214b30ba50d14b8c4b3987cfe4f
Florent Audonnet <2330834a@student.gla.ac.uk>
2020-03-11 19:42:20 +0000

    Added autoscrolling to text box Removed startup text when new text is added

--- fpd_explorer/frontend/home.py
+++ fpd_explorer/frontend/home.py
@@ -167,8 +167,11 @@ class ApplicationWindow(QMainWindow):
             del self.npz_path
             self._ui.npz_line.clear()
         self._files_loaded = False
-        del self.cyx
-        del self.ap
+        try:
+            del self.cyx
+            del self.ap
+        except Exception:
+            pass
         for _ in range(self._ui.tabWidget.count() - 1):
             # 1 because every time a tab is removed, indices are reassigned
             self._ui.tabWidget.removeTab(1)
