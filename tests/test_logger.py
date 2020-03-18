835dbdfa95adcca4b7ec8f304ade3b32ae1b1859
Ewan Pandelus <2319069P@student.gla.ac.uk>
2020-03-18 15:55:56 +0000

    tested add flag method

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 
 from fpd_explorer import logger
+from fpd_explorer.logger import Flags
 from fpd_explorer.frontend.home import ApplicationWindow
 from PySide2 import QtWidgets
 import fpd_explorer.config_handler as config
@@ -13,3 +14,17 @@ def test_add_flags_wrong_type():
     with pytest.raises(TypeError) as excinfo:
         logger.add_flag(5)
         assert "wrong type" in str(excinfo.value)
+
+
+def test_add_flags_wrong_value():
+    with pytest.raises(AttributeError) as excinfo:
+        logger.add_flag(Flags.wrongtype)
+        assert "wrong val" in str(excinfo.value)
+
+
+def test_add_correct_flag():
+    try:
+        logger.add_flag(Flags.phase_matching)
+        assert 1
+    except:
+        assert 0
