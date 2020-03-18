701e120ceddc603fdfee7e37415574febb843bf4
Ewan Pandelus <2319069P@student.gla.ac.uk>
2020-03-18 17:04:54 +0000

    tested wrong enum type in check_if_all_needed

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
-
+from test_buttons import setup_tests
 from fpd_explorer import logger
 from fpd_explorer.logger import Flags
 from fpd_explorer.frontend.home import ApplicationWindow
@@ -11,26 +11,28 @@ import pytest
 from pytestqt import qtbot
 
 
-def setup_tests(qtbot):
+def setup_logger_tests(qtbot):
     aw = ApplicationWindow()
     qtbot.addWidget(aw)
     logger.setup(aw._ui.log_text, aw)
 
 
-def test_add_flags_wrong_type():
-
+def test_add_flags_wrong_type(qtbot):
+    setup_logger_tests(qtbot)
     with pytest.raises(TypeError) as excinfo:
         logger.add_flag(5)
         assert "wrong type" in str(excinfo.value)
 
 
-def test_add_flags_wrong_value():
+def test_add_flags_wrong_value(qtbot):
+    setup_logger_tests(qtbot)
     with pytest.raises(AttributeError) as excinfo:
         logger.add_flag(Flags.wrongtype)
         assert "wrong val" in str(excinfo.value)
 
 
-def test_add_correct_flag():
+def test_add_correct_flag(qtbot):
+    setup_logger_tests(qtbot)
     try:
         logger.add_flag(Flags.phase_matching)
         assert 1
@@ -39,7 +41,14 @@ def test_add_correct_flag():
 
 
 def test_log_in_str(qtbot):
-    setup_tests(qtbot)
+    setup_logger_tests(qtbot)
     with pytest.raises(TypeError) as excinfo:
         logger.log(5)
         assert "wrong type in string " in str(excinfo.value)
+
+
+def test_if_all_needed(qtbot):
+    setup_tests(qtbot)
+    with pytest.raises(AttributeError) as excinfo:
+        logger.check_if_all_needed(Flags.wrongtype)
+        assert "wrong type" in str(excinfo.value)
